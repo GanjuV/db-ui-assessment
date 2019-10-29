1164e495c4f5d7530b278aaa182d7ce1127eb88e
Vaibhav Ganju <vaibhav34@gmail.com>
2019-10-29 15:46:47 +0530

    Updated the test cases

--- src/app/solution/solution.component.spec.ts
+++ src/app/solution/solution.component.spec.ts
@@ -1,8 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 import { SolutionComponent } from './solution.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { FormsModule, FormBuilder } from '@angular/forms';
+import { FormsModule, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 describe('SolutionComponent', () => {
   let component: SolutionComponent;
@@ -11,7 +10,7 @@ describe('SolutionComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SolutionComponent ],
-      imports: [ FormsModule ],
+      imports: [ FormsModule, ReactiveFormsModule ],
       schemas: [ NO_ERRORS_SCHEMA ],
       providers: [FormBuilder]
     })
@@ -26,7 +25,7 @@ describe('SolutionComponent', () => {
 
   // create reusable function for a dry spec.
   function updateForm(searchVal) {
-    component.formGroup.controls.searchVal.setValue(searchVal);
+    component.myForm.controls.searchVal.setValue(searchVal);
   }
 
   it('should create', () => {
@@ -42,28 +41,13 @@ describe('SolutionComponent', () => {
   it('should get error less than 3999 calling method', () => {
     updateForm(24242);
     component.getRomanData();
-    expect(component.outPut).toEqual('Number can not be more than 3999');
+    expect(component.outPut).toEqual('Number can not be greater than 3999');
   });
 
-  it('should get error not a valid number', () => {
-    const el: HTMLInputElement = fixture.debugElement.query(By.css('input')).nativeElement;
-    el.value = 'qweqweqwe';
-    el.dispatchEvent(new Event('input'));
-    expect(component.outPut).toBe('Enter number only');
-  });
-
-  it('should get error less than 3999', () => {
-    const el: HTMLInputElement = fixture.debugElement.query(By.css('input')).nativeElement;
-    el.value = '456607';
-    el.dispatchEvent(new Event('input'));
-    expect(component.outPut).toBe('Number can not be more than 3999');
-  });
-
-  it('should get roman number', () => {
-    const el: HTMLInputElement = fixture.debugElement.query(By.css('input')).nativeElement;
-    el.value = '435';
-    el.dispatchEvent(new Event('input'));
-    expect(component.outPut).toBe('CDLIII');
+  it('should get get out put as CXXIII', () => {
+    updateForm(123);
+    component.getRomanData();
+    expect(component.outPut).toEqual('CXXIII');
   });
 
 });
